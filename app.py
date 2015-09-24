3278753cf1a7cdb1dbe048d82e2ee52f08228b09
Daniel Craigmile <dcraigmile@texastribune.org>
2015-09-24 18:20:24 -0500

    set the port to listen on via the environment

--- app.py
+++ app.py
@@ -58,4 +58,5 @@ def charge():
     return render_template('charge.html', amount=amount)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
