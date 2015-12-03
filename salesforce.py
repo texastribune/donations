44945419d07cf90619334000842ec8c1a2a1a059
Daniel Craigmile <dcraigmile@texastribune.org>
2015-12-03 16:15:52 -0600

    move check_responses to its own file so it can easily be used in batch.py

--- salesforce.py
+++ salesforce.py
@@ -14,6 +14,7 @@ from config import SLACK_API_KEY
 from config import SLACK_CHANNEL
 
 from emails import send_email
+from check_response import check_response
 
 zone = timezone(TIMEZONE)
 
@@ -222,24 +223,6 @@ class SalesforceConnection(object):
             warn_multiple_accounts(email=email, count=len(response))
 
         return created, response[0]
-
-
-def check_response(response=None, expected_status=200):
-    """
-    Check the response from API calls to determine if they succeeded and
-    if not, why.
-    """
-    code = response.status_code
-    try:
-        content = json.loads(response.content.decode('utf-8'))
-        # TODO: look for 'success'
-        #print (content)
-    except:
-        print ('unable to parse response (this is probably okay)')
-    if code != expected_status:
-        print (content)
-        raise Exception('Expected {} but got {}'.format(expected_status, code))
-    return True
 
 
 def upsert_customer(customer=None, form=None):
