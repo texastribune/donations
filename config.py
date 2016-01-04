55b4064e5c0b1401167f6db1be56ffcdbda108e6
Daniel Craigmile <dcraigmile@texastribune.org>
2016-01-04 15:03:24 -0600

    try using crontab-style scheduling

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-# from celery.schedules import crontab
-from datetime import timedelta
+from celery.schedules import crontab
+#from datetime import timedelta
 
 import os
 
@@ -18,6 +18,8 @@ FLASK_SECRET_KEY = os.getenv('FLASK_SECRET_KEY')
 ########
 # Celery
 #
+
+WHEN = '4,16'
 CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
 CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND')
 CELERY_ALWAYS_EAGER = bool_env('CELERY_ALWAYS_EAGER')
@@ -25,7 +27,7 @@ CHARGE_MINUTES_FREQUENCY = int(os.getenv('CHARGE_MINUTES_FREQUENCY', 1440))
 CELERYBEAT_SCHEDULE = {
         'every-day': {
             'task': 'batch.charge_cards',
-            'schedule': timedelta(minutes=CHARGE_MINUTES_FREQUENCY)
+            'schedule': crontab(minute=0, hour=WHEN)
             },
         }
 ######
