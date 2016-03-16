59ae55e8e7137e8e94f15e233f7cd31d80b85b19
Kathryn Beaty <kabeaty@gmail.com>
2016-03-16 15:21:13 -0500

    Update to use format for print for consistency

--- app.py
+++ app.py
@@ -167,25 +167,25 @@ def charge():
                 email=request.form['stripeEmail'],
                 card=request.form['stripeToken']
         )
-        print('Create Stripe customer %s %s %s' % (customer_email,
+        print('Create Stripe customer {} {} {}'.format(customer_email,
             customer_first, customer_last))
     else:
         message = "There was an issue saving your email address."
-        print('Issue saving customer %s %s %s; showed error' % (customer_email,
+        print('Issue saving customer {} {} {}; showed error'.format(customer_email,
             customer_first, customer_last))
         return render_template('error.html', message=message)
 
     if form.validate():
         add_customer_and_charge.delay(form=request.form,
                 customer=customer)
-        print('Validated form of customer %s %s %s' % (customer_email,
+        print('Validated form of customer {} {} {}'.format(customer_email,
             customer_first, customer_last))
         return render_template('charge.html',
                 amount=request.form['amount'])
     else:
         message = "There was an issue saving your donation information."
-        print('Form validation errors: %s' % form.errors)
-        print('Did not validate form of customer %s %s %s' % (customer_email,
+        print('Form validation errors: {}'.format(form.errors))
+        print('Did not validate form of customer {} {} {}'.format(customer_email,
             customer_first, customer_last))
         return render_template('error.html', message=message)
 
