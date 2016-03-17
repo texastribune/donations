97cab82879377f009961ad5b3b21cabbd07a233e
Daniel Craigmile <dcraigmile@texastribune.org>
2016-03-17 18:58:38 -0500

    lock the charge_cards job from executing concurrently with another one

--- batch.py
+++ batch.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 import json
+import redis
 
 import celery
 from emails import send_email
@@ -11,6 +12,7 @@ from salesforce import SalesforceConnection
 from config import STRIPE_KEYS
 from config import ACCOUNTING_MAIL_RECIPIENT
 from config import TIMEZONE
+from config import REDIS_URL
 
 zone = timezone(TIMEZONE)
 
@@ -121,8 +123,36 @@ def process_charges(query, log):
             raise Exception('problem')
 
 
+class AlreadyExecuting(Exception):
+    """
+    Here to show when more than one job of the same type is running.
+    """
+    pass
+
+
+class Lock(object):
+    """
+    Claim an exclusive lock. Using Redis.
+    """
+
+    def __init__(self, key):
+        self.key = key
+        self.connection = redis.from_url(REDIS_URL)
+
+    def acquire(self):
+        if self.connection.get(self.key):
+            raise AlreadyExecuting
+        self.connection.setex(name=self.key, value='bar', time=1200)
+
+    def release(self):
+        self.connection.delete(self.key)
+
+
 @celery.task()
 def charge_cards():
+
+    lock = Lock(key='charge-cards-lock')
+    lock.acquire()
 
     log = Log()
 
@@ -172,6 +202,7 @@ def charge_cards():
     process_charges(query, log)
     log.send()
 
+    lock.release()
 
 if __name__ == '__main__':
     charge_cards()
