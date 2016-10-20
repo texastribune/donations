1ddd3d74f36de7486bca3db30ec2563d3fe64cdc
Daniel Craigmile <dcraigmile@texastribune.org>
2016-10-19 23:22:17 -0500

    see; this is why

--- salesforce.py
+++ salesforce.py
@@ -434,7 +434,7 @@ def _format_blast_rdo(contact=None, form=None, customer=None):
     amount = form['amount']
     installments = 0
     open_ended_status = 'Open'
-    print form
+    pprint(form)
 
     if form['pay_fees_value'] == 'True':
         pay_fees = True
