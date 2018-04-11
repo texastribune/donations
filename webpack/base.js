fb3cbf4b2a830cca86965c4367879d81ec6bad12
AndrewGibson27 <atgibson27@gmail.com>
2018-04-11 18:18:31 -0500

    fix typo

--- webpack/base.js
+++ webpack/base.js
@@ -3,7 +3,7 @@ const path = require('path');
 const entryDir = path.join(process.cwd(), 'static', 'js', 'entry');
 const buildDir = path.join(process.cwd(), 'static', 'js', 'build');
 
-consolel.log(path.resolve(__dirname, './dist'));
+console.log(path.resolve(__dirname, './dist'));
 console.log(entryDir, buildDir);
 
 module.exports = { entryDir, buildDir };
