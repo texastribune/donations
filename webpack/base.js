309c0e78c7e0a620c5e6afb26d383e695cf888f1
AndrewGibson27 <atgibson27@gmail.com>
2018-04-11 18:07:46 -0500

    try referencing the root path differently for JS builds

--- webpack/base.js
+++ webpack/base.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
-const entryDir = path.join(process.cwd(), 'static', 'js', 'entry');
-const buildDir = path.join(process.cwd(), 'static', 'js', 'build');
+const entryDir = path.join(process.cwd(), 'app', 'static', 'js', 'entry');
+const buildDir = path.join(process.cwd(), 'app', 'static', 'js', 'build');
 
 module.exports = { entryDir, buildDir };
