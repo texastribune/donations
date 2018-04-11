98a9aaf0f3ae0e40ec974de0f78a77b673fe0831
AndrewGibson27 <atgibson27@gmail.com>
2018-04-11 18:16:45 -0500

    try printing the paths

--- webpack/base.js
+++ webpack/base.js
@@ -1,6 +1,9 @@
 const path = require('path');
 
-const entryDir = path.join(process.cwd(), 'app', 'static', 'js', 'entry');
-const buildDir = path.join(process.cwd(), 'app', 'static', 'js', 'build');
+const entryDir = path.join(process.cwd(), 'static', 'js', 'entry');
+const buildDir = path.join(process.cwd(), 'static', 'js', 'build');
+
+consolel.log(path.resolve(__dirname, './dist'));
+console.log(entryDir, buildDir);
 
 module.exports = { entryDir, buildDir };
