293a713981cadadf9910530c2d9712c36268fd00
AndrewGibson27 <atgibson27@gmail.com>
2018-05-02 14:22:58 -0500

    create frequency component

---
+++ static/js/src/elements/Frequency.vue
@@ -0,0 +1,33 @@
+<template>
+  <fieldset>
+    <input
+      v-for="option in options"
+      :key="option.id"
+      :value="option.value"
+      :name="identifier"
+      :checked="value === option.value"
+      type="radio"
+      @input="updateValue($event.target.value)"
+    >
+  </fieldset>
+</template>
+
+<script>
+import connectedFormElement from '../mixins/connectedFormElement';
+
+export default {
+  name: 'Frequency',
+
+  mixins: [connectedFormElement],
+
+  data() {
+    return {
+      options: [
+        { id: 0, text: 'monthly', value: 'monthly' },
+        { id: 1, text: 'yearly', value: 'yearly' },
+        { id: 2, text: 'one time', value: 'None' },
+      ],
+    };
+  },
+};
+</script>
--- static/js/src/entry/donate/TopForm.vue
+++ static/js/src/entry/donate/TopForm.vue
@@ -7,17 +7,24 @@
       identifier="amount"
       store-module="baseForm"
     />
+
+    <frequency
+      identifier="installment_period"
+      store-module="baseForm"
+    />
   </form>
 </template>
 
 <script>
 import Amount from '../../elements/Amount.vue';
+import Frequency from '../../elements/Frequency.vue';
 
 export default {
   name: 'TopForm',
 
   components: {
     Amount,
+    Frequency,
   },
 };
 </script>
