e7cfd1d2957a90fe657f2a4610e62788bd5e7df6
AndrewGibson27 <atgibson27@gmail.com>
2018-05-29 17:13:45 -0500

    clean up for markup

--- static/js/src/entry/donate/TopForm.vue
+++ static/js/src/entry/donate/TopForm.vue
@@ -6,165 +6,171 @@
     class="form"
     @submit="$event.preventDefault()"
   >
-    <div>
-      <div>
-        <div class="form__group">
-          <div class="grid_row grid_separator">
-            <div class="col">
-              <radios
-                :options="frequencyOptions"
-                base-classes="grid_row form__radios"
-                name="installment_period"
-                store-module="baseForm"
-                @updateCallback="onFrequencyUpdate"
-              />
-            </div>
-          </div>
-          <div class="grid_row grid_separator--xs">
-            <div class="col">
-              <text-input
-                :show-error="showAllErrors"
-                :validation="validation.amount"
-                has-label
-                label-text="amount ($)"
-                base-classes="form__text form__text--heavy"
-                error-classes="form__error"
-                label-classes="form__label grid_separator--xs"
-                name="amount"
-                store-module="baseForm"
-                @markErrorValidity="markErrorValidity"
-              />
-            </div>
-          </div>
-          <div class="grid_separator">
-            <p class="subtext">For three-year commitments of $1,000 or more, join our <a href="https://support.texastribune.org/circle.html">Circle Membership program</a>.</p>
-          </div>
-        </div>
+    <div class="grid_row grid_separator">
+      <div class="col">
+        <radios
+          :options="frequencyOptions"
+          base-classes="grid_row form__radios"
+          name="installment_period"
+          store-module="baseForm"
+          @updateCallback="onFrequencyUpdate"
+        />
+      </div>
+    </div>
 
-        <div class="form__group">
-          <div class="grid_row grid_separator">
-            <div class="col">
-              <text-input
-                :show-error="showAllErrors"
-                :validation="validation.stripeEmail"
-                has-label
-                label-text="email address"
-                type="email"
-                base-classes="form__text form__text--standard"
-                error-classes="form__error"
-                label-classes="form__label grid_separator--xs"
-                name="stripeEmail"
-                store-module="baseForm"
-                @markErrorValidity="markErrorValidity"
-              />
-            </div>
-          </div>
-          <div class="grid_row grid_wrap--s">
-            <div class="col_6 grid_separator">
-              <text-input
-                :show-error="showAllErrors"
-                :validation="validation.first_name"
-                has-label
-                label-text="first name"
-                base-classes="form__text form__text--standard"
-                error-classes="form__error"
-                label-classes="form__label grid_separator--xs"
-                name="first_name"
-                store-module="baseForm"
-                @markErrorValidity="markErrorValidity"
-              />
-            </div>
-            <div class="col_6 grid_separator">
-              <text-input
-                :show-error="showAllErrors"
-                :validation="validation.last_name"
-                has-label
-                label-text="last name"
-                base-classes="form__text form__text--standard"
-                error-classes="form__error"
-                label-classes="form__label grid_separator--xs"
-                name="last_name"
-                store-module="baseForm"
-                @markErrorValidity="markErrorValidity"
-              />
-            </div>
-          </div>
+    <div class="grid_row grid_separator--xs">
+      <div class="col">
+        <text-input
+          :show-error="showAllErrors"
+          :validation="validation.amount"
+          has-label
+          label-text="amount ($)"
+          base-classes="form__text form__text--heavy"
+          error-classes="form__error"
+          label-classes="form__label grid_separator--xs"
+          name="amount"
+          store-module="baseForm"
+          @markErrorValidity="markErrorValidity"
+        />
+      </div>
+    </div>
 
-          <div class="grid_row grid_wrap--s">
-            <div class="col_6 grid_separator">
-              <text-input
-                :required="false"
-                has-label
-                label-text="encouraged to give by"
-                base-classes="form__text form__text--standard"
-                error-classes="form__error"
-                label-classes="form__label grid_separator--xs"
-                name="reason"
-                store-module="baseForm"
-              />
-            </div>
-            <div class="col_6 grid_separator">
-              <text-input
-                :required="false"
-                :show-error="showAllErrors"
-                :validation="validation.zipcode"
-                has-label
-                label-text="zip code"
-                maxlength="5"
-                base-classes="form__text form__text--standard"
-                error-classes="form__error"
-                label-classes="form__label grid_separator--xs"
-                name="zipcode"
-                store-module="baseForm"
-                @markErrorValidity="markErrorValidity"
-              />
-            </div>
-          </div>
+    <div class="grid_row grid_separator">
+      <div class="col">
+        <p class="subtext">For three-year commitments of $1,000 or more, join our <a href="https://support.texastribune.org/circle.html">Circle Membership program</a>.</p>
+      </div>
+    </div>
+
+    <div class="grid_row grid_separator">
+      <div class="col">
+        <text-input
+          :show-error="showAllErrors"
+          :validation="validation.stripeEmail"
+          has-label
+          label-text="email address"
+          type="email"
+          base-classes="form__text form__text--standard"
+          error-classes="form__error"
+          label-classes="form__label grid_separator--xs"
+          name="stripeEmail"
+          store-module="baseForm"
+          @markErrorValidity="markErrorValidity"
+        />
+      </div>
+    </div>
+
+    <div class="grid_row grid_wrap--s">
+      <div class="col_6 grid_separator">
+        <text-input
+          :show-error="showAllErrors"
+          :validation="validation.first_name"
+          has-label
+          label-text="first name"
+          base-classes="form__text form__text--standard"
+          error-classes="form__error"
+          label-classes="form__label grid_separator--xs"
+          name="first_name"
+          store-module="baseForm"
+          @markErrorValidity="markErrorValidity"
+        />
+      </div>
+      <div class="col_6 grid_separator">
+        <text-input
+          :show-error="showAllErrors"
+          :validation="validation.last_name"
+          has-label
+          label-text="last name"
+          base-classes="form__text form__text--standard"
+          error-classes="form__error"
+          label-classes="form__label grid_separator--xs"
+          name="last_name"
+          store-module="baseForm"
+          @markErrorValidity="markErrorValidity"
+        />
+      </div>
+    </div>
+
+    <div class="grid_row grid_wrap--s">
+      <div class="col_6 grid_separator">
+        <text-input
+          :required="false"
+          has-label
+          label-text="encouraged to give by"
+          base-classes="form__text form__text--standard"
+          error-classes="form__error"
+          label-classes="form__label grid_separator--xs"
+          name="reason"
+          store-module="baseForm"
+        />
+      </div>
+      <div class="col_6 grid_separator">
+        <text-input
+          :required="false"
+          :show-error="showAllErrors"
+          :validation="validation.zipcode"
+          has-label
+          label-text="zip code"
+          maxlength="5"
+          base-classes="form__text form__text--standard"
+          error-classes="form__error"
+          label-classes="form__label grid_separator--xs"
+          name="zipcode"
+          store-module="baseForm"
+          @markErrorValidity="markErrorValidity"
+        />
+      </div>
+    </div>
+
+    <div class="grid_row grid_separator">
+      <div class="col">
+        <pay-fees
+          base-classes="form__fees"
+          amount-store-module="baseForm"
+          pay-fees-value-store-module="baseForm"
+        />
+      </div>
+    </div>
+
+    <div :class="nativeIsSupported ? 'grid_separator--l' : ''">
+      <div class="grid_row">
+        <div class="col">
+          <native-pay
+            :form-is-valid="nativeIsValid"
+            :supported="nativeIsSupported"
+            base-classes="form__native"
+            amount-store-module="baseForm"
+            token-store-module="baseForm"
+            @setValue="setValue"
+            @onSubmit="onSubmit"
+          />
         </div>
       </div>
+    </div>
 
-      <div class="donate-form__payment">
-        <div>
-          <div class="grid_separator--l">
-            <pay-fees
-              base-classes="form__fees"
-              amount-store-module="baseForm"
-              pay-fees-value-store-module="baseForm"
-            />
-          </div>
-          <div :class="showManualPay ? 'grid_separator--l' : ''">
-            <native-pay
-              :form-is-valid="nativeIsValid"
-              :supported="nativeIsSupported"
-              base-classes="form__native"
-              amount-store-module="baseForm"
+    <div :aria-live="nativeIsSupported ? 'polite' : false">
+      <div v-if="showManualPay">
+        <div class="grid_row">
+          <div class="col">
+            <manual-pay
+              :show-error="showManualErrors"
+              :validation="validation.card"
+              base-classes="form__manual"
               token-store-module="baseForm"
-              @setValue="setValue"
-              @onSubmit="onSubmit"
+              error-classes="form__error"
+              @markErrorValidity="markErrorValidity"
             />
           </div>
-          <div
-            :aria-live="nativeIsSupported ? 'polite' : false"
-          >
-            <div v-if="showManualPay">
-              <manual-pay
-                :show-error="showManualErrors"
-                :validation="validation.card"
-                base-classes="form__manual"
-                token-store-module="baseForm"
-                error-classes="form__error"
-                @markErrorValidity="markErrorValidity"
-              />
-              <div class="grid_row">
-                <manual-submit
-                  :form-is-valid="manualIsValid"
-                  base-classes="col button button--yellow button--l"
-                  value="Pay by card"
-                  @onSubmit="onSubmit"
-                  @setValue="setValue"
-                />
-              </div>
-            </div>
+        </div>
+
+        <div class="grid_row">
+          <div class="col">
+            <manual-submit
+              :form-is-valid="manualIsValid"
+              base-classes="form__submit button button--yellow button--l"
+              value="Pay by card"
+              @onSubmit="onSubmit"
+              @setValue="setValue"
+            />
           </div>
         </div>
       </div>
