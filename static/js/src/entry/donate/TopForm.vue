2597f50e8f3aad34e3c3079773fdfeae609da646
AndrewGibson27 <atgibson27@gmail.com>
2018-06-05 12:32:30 -0500

    jk about the email and amount on same row

--- static/js/src/entry/donate/TopForm.vue
+++ static/js/src/entry/donate/TopForm.vue
@@ -18,8 +18,8 @@
       </div>
     </div>
 
-    <div class="grid_row grid_wrap--s">
-      <div class="col_6 grid_separator">
+    <div class="grid_row grid_separator--xs">
+      <div class="col">
         <text-input
           :show-error="showManualErrors || showNativeErrors"
           :validation="validation.amount"
@@ -31,7 +31,16 @@
           @setValidationValue="setValidationValue"
         />
       </div>
-      <div class="col_6 grid_separator">
+    </div>
+
+    <div class="grid_row grid_separator">
+      <div class="col">
+        <p class="subtext">For three-year commitments of $1,000 or more, join our <a href="https://support.texastribune.org/circle.html">Circle Membership program</a>.</p>
+      </div>
+    </div>
+
+    <div class="grid_row grid_separator">
+      <div class="col">
         <text-input
           :show-error="showManualErrors || showNativeErrors"
           :validation="validation.stripeEmail"
@@ -43,12 +52,6 @@
           store-module="baseForm"
           @setValidationValue="setValidationValue"
         />
-      </div>
-    </div>
-
-    <div class="grid_row grid_separator">
-      <div class="col">
-        <p class="subtext">For three-year commitments of $1,000 or more, join our <a href="https://support.texastribune.org/circle.html">Circle Membership program</a>.</p>
       </div>
     </div>
 
