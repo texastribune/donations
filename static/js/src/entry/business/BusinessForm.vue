8be1b3a234574ff20fbfb05ed068b7a78d737650
Val Asensio <vasensio@texastribune.org>
2018-10-18 09:56:15 -0500

    Remove DEFAULT_STATE_SELECTED from BusMem form, moved to form state hydration.

--- static/js/src/entry/business/BusinessForm.vue
+++ static/js/src/entry/business/BusinessForm.vue
@@ -297,7 +297,7 @@ import updateStoreValue from '../../elements/mixins/updateStoreValue';
 import formStarter from '../../mixins/form/starter';
 // import addNumberCommas from '../../utils/addNumberCommas';
 import { US_STATES_SELECT_LIST } from '../../utils/formSelectListConstants';
-import { DEFAULT_STATE_SELECTED } from './constants';
+//import { DEFAULT_STATE_SELECTED } from './constants';
 
 
 export default {
@@ -385,7 +385,7 @@ export default {
       },
       options: {
         list: this.buildList(US_STATES_SELECT_LIST),
-        default: DEFAULT_STATE_SELECTED,
+        // default: DEFAULT_STATE_SELECTED,
       },
     };
   },
