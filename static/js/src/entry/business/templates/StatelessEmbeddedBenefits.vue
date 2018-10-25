1f7b905146e9341307b5b36e15b019c36db5be3f
Val Asensio <vasensio@texastribune.org>
2018-10-25 17:03:33 -0500

    Spacing tweaks; added the link for donation pdf.

--- static/js/src/entry/business/templates/StatelessEmbeddedBenefits.vue
+++ static/js/src/entry/business/templates/StatelessEmbeddedBenefits.vue
@@ -5,25 +5,22 @@
     </header>
     <h4 class="smallcaps--light grid_separator--xs">Included with every business membership</h4>
     <p class="business-benefits__item grid_separator--s">Organization name featured on texastribune.org support
-      page, Business Membership tab, with link to business site</p>
+    page, Business Membership tab, with link to business site</p>
     <p class="business-benefits__item grid_separator--s">Organization name featured in a thank you segment of The
-      Brief, once annually</p>
+    Brief, once annually</p>
     <p class="business-benefits__item grid_separator--s">Included in distribution list for TribMonth, an exclusive
-      communication</p>
+    communication</p>
     <p class="business-benefits__item grid_separator--s">First opporttunity to RSVP for select Texas Tribune
-      events</p>
+    events</p>
     <p class="business-benefits__item grid_separator--s">Special fee to reserve the Tribune's event studio.</p>
     <p class="business-benefits__item grid_separator--s">Discount for Texas Tribune Festival pass</p>
     <p class="business-benefits__item grid_separator--s">Texas Tribune Business Membership window decal or 4x6
-      printed card</p>
+    printed card</p>
     <h4 class="smallcaps--light grid_separator--xs">At higher levels</h4>
     <p class="business-benefits__item grid_separator--s">Annual subscription to The Blast, the best daily premium
-      poltical newsleter in Texas, for insiders who want the behind-the-scenes scoop</p>
+    poltical newsleter in Texas, for insiders who want the behind-the-scenes scoop</p>
     <p class="business-benefits__item grid_separator">If you prefer more choice, via a custom marketing / messaging
-      program, learn more at advertise or contact April Hinkle directly at ahinkle@texastribune.org</p>
-    <h4 className="smallcaps--light grid_separator--xs">Donate by mail</h4>
-    <p className="circle-benefits__header link--blue grid_separator">You can also <a
-      href="{{ url_for('static', filename='pdf/business-membership-form.pdf') }}">donate by mail</a>.</p>
+    program, learn more at advertise or contact April Hinkle directly at ahinkle@texastribune.org</p>
   </section>
 </template>
 
