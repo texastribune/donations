bc6f50feed7b6444b2f7820713152b2cce34c67f
Val Asensio <vasensio@texastribune.org>
2018-11-05 12:42:39 -0600

    Fix commented out duplicate code.

--- static/js/src/entry/business/router.js
+++ static/js/src/entry/business/router.js
@@ -101,7 +101,6 @@ function createInitialFormState(queryParams) {
 function createRouter() {
   return new VueRouter({
     base: '/business',
-    //     base: '/business',
     mode: 'history',
     routes: [
       { path: '/', component: RouteHandler },
