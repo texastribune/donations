a076a9749e0baf162215b462b60b87f11f240988
Daniel Craigmile <danielc@pobox.com>
2019-01-05 17:47:17 -0600

    change Slack notifications to use attachments

--- util.py
+++ util.py
@@ -73,34 +73,36 @@ def notify_slack(contact=None, opportunity=None, rdo=None, account=None):
     )
     message = {
         "channel": SLACK_CHANNEL,
-        "attachments": [attachment],
+        "attachments": json.dumps([attachment]),
         "icon_emoji": ":moneybag:",
     }
     send_slack_message(message=message)
-    return
 
-    text = construct_slack_message(
-        contact=contact, opportunity=opportunity, rdo=rdo, account=account
-    )
-    message = {
-        "text": text,
-        "channel": SLACK_CHANNEL,
-        "username": opportunity.lead_source,
-        "icon_emoji": ":moneybag:",
-    }
+    # text = construct_slack_message(
+    #     contact=contact, opportunity=opportunity, rdo=rdo, account=account
+    # )
+    # message = {
+    #     "text": text,
+    #     "channel": SLACK_CHANNEL,
+    #     "username": opportunity.lead_source,
+    #     "icon_emoji": ":moneybag:",
+    # }
 
-    send_slack_message(message)
+    # send_slack_message(message)
 
 
-def send_slack_message(message=None):
+def send_slack_message(message=None, username="moneybot"):
 
     if not ENABLE_SLACK or not message:
         return
-
     message["token"] = SLACK_API_KEY
+    message["username"] = username
     url = "https://slack.com/api/chat.postMessage"
     try:
-        requests.get(url, params=message)
+        response = requests.post(url, params=message)
+        slack_response = json.loads(response.text)
+        if not slack_response["ok"]:
+            raise Exception(slack_response["error"])
     except Exception as e:
         logging.error(f"Failed to send Slack notification: {e}")
 
@@ -115,95 +117,31 @@ def construct_slack_attachment(
     reason=None,
 ):
     attachment = {
-        # "pretext"
         "fallback": "Donation",
         "color": "good",
-        # "text": "text",
+        # "pretext": "optional and appears above attachment"
+        # # "text": "text",
         "author_name": donor,
         # author_link
         # author_icon
         "title": email,
+        # "title_link":
+        # "text": reason,
         # title_link
         "fields": [
-            {"title": "Source", "value": source, "short": False},
+            {"title": "Source", "value": source, "short": True},
             {"title": "Amount", "value": f"${amount}", "short": True},
-            {"title": "Period", "value": period, "short": False},
-            {"title": "Type", "value": donation_type, "short": False},
-            {"title": "Reason", "value": reason, "short": False},
+            {"title": "Period", "value": period, "short": True},
+            {"title": "Type", "value": donation_type, "short": True},
         ],
+        # "image_url":
+        # "thumb_url":
+        # "footer":
+        # "footer_icon":
+        # "ts":
     }
 
-    if reason:
-        attachment["fields"].append(
-            {"title": "Reason", "value": reason, "short": False}
-        )
-        pprint(attachment, indent=2)
     return attachment
-
-
-def send_slack_attachment(
-    channel=SLACK_CHANNEL, attachment=None, username="moneybot", icon_emoji=":moneybag:"
-):
-
-    if not ENABLE_SLACK:
-        return
-    payload = {
-        "channel": channel,
-        "attachments": attachment,
-        "username": "Foo Bar",
-        "icon_emoji": ":moneybag:",
-    }
-    pprint(payload)
-    url = "https://slack.com/api/chat.postMessage"
-    headers = {
-        "Authorization": f"Bearer {SLACK_API_KEY}",
-        "Content-type": "application/json; charset=utf-8",
-    }
-    try:
-        response = requests.post(url, headers=headers, json=payload)
-    except Exception as e:
-        logging.error(f"Failed to send Slack notification: {e}")
-    pprint(response.text, indent=2)
-
-    # {
-    # 	"icon_emoji": ":moneybag:",
-    # 	"attachments": [
-    #         {
-    #             "fallback": "Required plain-text summary of the attachment.",
-    #             "color": "#2eb886",
-    #             "pretext": "Optional text that appears above the attachment block",
-    #             "author_name": "Bobby Tables",
-    #             "author_link": "http://flickr.com/bobby/",
-    #             "author_icon": "http://flickr.com/icons/bobby.jpg",
-    #             "title": "Your Name Here",
-    #             "title_link": "https://api.slack.com/",
-    #             "text": "Optional text that appears within the attachment",
-    #              "fields": [
-    #                 {"title": "Source", "value": "Amazon Alexa", "short": true},
-    #                 {"title": "Amount", "value": "$5", "short": true},
-    #                 {"title": "Period", "value": "yearly", "short": true},
-    #                 {"title": "Type", "value": "Circle", "short": true}
-    #             ],
-    #             "image_url": "http://my-website.com/path/to/image.jpg",
-    #             "thumb_url": "http://example.com/path/to/thumb.png",
-    #             "footer": "Slack API",
-    #             "footer_icon": "https://platform.slack-edge.com/img/default_application_icon.png",
-    #             "ts": 123456789
-    #         }
-    #     ]
-    # }
-    # attachment = [
-    #     {
-    #         "fallback": "Deployment status.",
-    #         "color": "good",
-    #         "mrkdwn_in": ["text"],
-    #         "title": "instance_name",
-    #         "fields": [
-    #             {"value": "status", "short": False},
-    #             {"value": "Command ID: foo", "short": False},
-    #         ],
-    #     }
-    # ]
 
 
 def send_multiple_account_warning(contact):
