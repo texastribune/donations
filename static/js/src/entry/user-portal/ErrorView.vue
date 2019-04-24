f04543c4288bca91938c62d66ec23cd178c29505
AndrewGibson27 <atgibson27@gmail.com>
2019-04-24 16:14:29 -0500

    improve error handling

---
+++ static/js/src/entry/user-portal/ErrorView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Sorry about that.</h1>
+    <p>An error occurred, and we're working hard to fix it.</p>
+    <p>
+      You can return <a href="/user-portal">home</a> or email
+      <a href="mailto:community@texastribune.org">community@texastribune.org</a>
+      if the issue persists.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Error',
+};
+</script>
