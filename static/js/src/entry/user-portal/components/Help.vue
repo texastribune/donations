bd1d3b565715a080b1c7df4528e5d593cdbcf36d
AndrewGibson27 <atgibson27@gmail.com>
2019-05-07 15:20:18 -0500

    allow for different help text

--- static/js/src/entry/user-portal/components/Help.vue
+++ static/js/src/entry/user-portal/components/Help.vue
@@ -1,9 +1,16 @@
 <template>
   <section>
     <h2>Need help?</h2>
-    <p>
+    <p v-if="withFeedback">
+      If you have questions about your Texas Tribune account or would like to
+      give us feedback on this experience, please contact us at
+      <a href="mailto:community@texastribune.org">community@texastribune.org</a
+      >.
+    </p>
+    <p v-else>
       To update your membership status, contact us at
-      <a href="mailto:community@texastribune.org">community@texastribune.org</a>
+      <a href="mailto:community@texastribune.org">community@texastribune.org</a
+      >.
     </p>
   </section>
 </template>
@@ -11,5 +18,12 @@
 <script>
 export default {
   name: 'Help',
+
+  props: {
+    withFeedback: {
+      type: Boolean,
+      default: false,
+    },
+  },
 };
 </script>
