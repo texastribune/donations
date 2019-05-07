5cee79e7bca570d198bb37630842a122fa6c0a7e
AndrewGibson27 <atgibson27@gmail.com>
2019-05-07 14:12:28 -0500

    create generic help component

---
+++ static/js/src/entry/user-portal/components/Help.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <h2>Need help?</h2>
+    <p>
+      To update your membership status, contact us at
+      <a href="mailto:community@texastribune.org">community@texastribune.org</a>
+    </p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Help',
+};
+</script>
