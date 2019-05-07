9e327457b29ca65c199e9ee56d8b6203fcdfc06e
AndrewGibson27 <atgibson27@gmail.com>
2019-05-07 15:45:59 -0500

    create summary-box reusable component

---
+++ static/js/src/entry/user-portal/components/SummaryBox.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="c-summary-box">
+    <h2>{{ heading }}</h2>
+    <slot name="content"></slot> <slot name="links"></slot>
+    <div v-if="!!$slots.bottom" class=""><slot name="bottom"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SummaryBox',
+
+  props: {
+    heading: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
