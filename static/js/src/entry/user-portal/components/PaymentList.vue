e15480eb4300f9bb26e1d1c1e3b4e614dc3773ed
AndrewGibson27 <atgibson27@gmail.com>
2019-05-07 14:33:45 -0500

    create generic payment list component

---
+++ static/js/src/entry/user-portal/components/PaymentList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <table class="c-table c-table--bordered l-width-full">
+      <thead>
+        <tr>
+          <th><strong>Date</strong></th>
+          <th><strong>Amount</strong></th>
+          <th><strong>Payment</strong></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="payment in payments" :key="payment.id">
+          <td>
+            <slot name="payment" :date="payment.date">
+              {{ payment.date }}
+            </slot>
+          </td>
+          <td>
+            <slot name="amount" :date="payment.amount">
+              {{ payment.amount }}
+            </slot>
+          </td>
+          <td>
+            <slot name="method" :date="payment.method">
+              {{ payment.method }}
+            </slot>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <button type="button" @click="$emit('onLoadMore')">Load more</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PaymentList',
+
+  props: {
+    payments: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
