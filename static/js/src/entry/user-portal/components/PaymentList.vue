b1da460317497d0e881382b93318a0ab42581793
AndrewGibson27 <atgibson27@gmail.com>
2019-05-20 15:27:53 -0500

    add styles to payment-history list

--- static/js/src/entry/user-portal/components/PaymentList.vue
+++ static/js/src/entry/user-portal/components/PaymentList.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
+  <div class="c-payments has-more l-width-full">
     <table class="c-table c-table--bordered l-width-full">
       <thead>
         <tr>
-          <th><strong>Date</strong></th>
-          <th><strong>Amount</strong></th>
-          <th><strong>Payment</strong></th>
+          <th class="t-align-left"><strong>Date</strong></th>
+          <th class="t-align-left"><strong>Amount</strong></th>
+          <th class="t-align-left"><strong>Payment</strong></th>
         </tr>
       </thead>
       <tbody>
@@ -26,7 +26,13 @@
         </tr>
       </tbody>
     </table>
-    <button type="button" @click="$emit('onLoadMore')">Load more</button>
+    <button
+      class="c-button c-button--s has-bg-white has-box-shadow"
+      type="button"
+      @click="$emit('onLoadMore')"
+    >
+      Load more
+    </button>
   </div>
 </template>
 
