561c864570bb79c7ab8f44b97730e471c538bb0a
AndrewGibson27 <atgibson27@gmail.com>
2019-06-05 16:30:02 -0500

    a few small tweaks

--- static/js/src/entry/user-portal/components/Appeal.vue
+++ static/js/src/entry/user-portal/components/Appeal.vue
@@ -19,10 +19,7 @@
         }"
         class="c-appeal__item"
       >
-        <icon
-          :name="index <= ceiling ? 'check' : 'close'"
-          :display="{ size: 'n' }"
-        />
+        <icon :name="index <= ceiling ? 'check' : 'close'" />
         <component :is="benefit.component" />
       </li>
     </ul>
