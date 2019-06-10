c4911a9883992cd63afcebd47b02a5b500f61c71
AndrewGibson27 <atgibson27@gmail.com>
2019-06-10 17:56:30 -0500

    add some text to our loading wheel

--- static/js/src/entry/user-portal/components/Loader.vue
+++ static/js/src/entry/user-portal/components/Loader.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="c-full-page-loader">
-    <div class="l-align-center-y"><div class="c-loading c-loading--s" /></div>
+    <div class="l-align-center-y has-xl-padding">
+      <div class="c-loading c-loading--tight has-b-btm-marg" />
+      <p class="has-text-gray t-align-center t-size-m">
+        <strong>Building your dashboard &hellip;</strong>
+      </p>
+    </div>
   </div>
 </template>
 
