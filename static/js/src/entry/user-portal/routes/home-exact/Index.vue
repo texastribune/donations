3feea3c28b81448532333225d218d2500033f8d9
AndrewGibson27 <atgibson27@gmail.com>
2019-06-11 13:04:59 -0500

    make messages different depending on user status

--- static/js/src/entry/user-portal/routes/home-exact/Index.vue
+++ static/js/src/entry/user-portal/routes/home-exact/Index.vue
@@ -1,28 +1,6 @@
 <template>
   <div class="has-white-bg-from-bp-l has-ump-top-padding">
-    <messages :num-messages="1">
-      <template v-slot="slotProps">
-        <message
-          heading="Welcome"
-          :name="readOnlyWelcomeMessageKey"
-          @setMessageSeen="slotProps.setMessageSeen"
-        >
-          <template v-slot:icon>
-            <icon name="bell" :display="{ size: 's' }" />
-          </template>
-          <template v-slot:content>
-            <p class="has-text-gray-dark t-space-heading-m t-size-s">
-              <em>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-              </em>
-            </p>
-          </template>
-        </message>
-      </template>
-    </messages>
+    <messages />
 
     <h1 class="has-l-btm-marg has-ump-side-padding t-size-xl">Your Account</h1>
 
@@ -41,16 +19,13 @@
 <script>
 import Help from '../../components/Help.vue';
 import routeMixin from '../../mixins/route';
-import userMixin from '../home/mixins/user';
-import { READ_ONLY_WELCOME_MESSAGE_KEY } from '../../constants';
 import ContactInfo from './containers/ContactInfoContainer.vue';
 import Membership from './containers/MembershipContainer.vue';
 import Expired from './containers/ExpiredContainer.vue';
 import OneTime from './containers/OneTimeContainer.vue';
 import NeverGiven from './containers/NeverGivenContainer.vue';
 import Blast from './containers/BlastContainer.vue';
-import Messages from './components/Messages.vue';
-import Message from './components/Message.vue';
+import Messages from './containers/MessagesContainer.vue';
 
 export default {
   name: 'HomeExactRoute',
@@ -63,16 +38,9 @@ export default {
     NeverGiven,
     Blast,
     Messages,
-    Message,
     Help,
   },
 
-  mixins: [routeMixin, userMixin],
-
-  data() {
-    return {
-      readOnlyWelcomeMessageKey: READ_ONLY_WELCOME_MESSAGE_KEY,
-    };
-  },
+  mixins: [routeMixin],
 };
 </script>
---
+++ static/js/src/entry/user-portal/routes/home-exact/containers/MessagesContainer.vue
@@ -0,0 +1,41 @@
+<template>
+  <messages-wrapper
+    :is-recurring="isRecurring"
+    :is-blast="isBlast"
+    :is-never-given="isNeverGiven"
+  />
+</template>
+
+<script>
+/* eslint-disable camelcase */
+
+import userMixin from '../../home/mixins/user';
+import MessagesWrapper from '../components/MessagesWrapper.vue';
+
+export default {
+  name: 'MessagesContainer',
+
+  components: { MessagesWrapper },
+
+  mixins: [userMixin],
+
+  computed: {
+    isRecurring() {
+      return this.user.recurring_donor;
+    },
+
+    isBlast() {
+      const {
+        is_former_blast_subscriber,
+        is_current_blast_subscriber,
+      } = this.user;
+
+      return is_former_blast_subscriber || is_current_blast_subscriber;
+    },
+
+    isNeverGiven() {
+      return this.user.never_given;
+    },
+  },
+};
+</script>
