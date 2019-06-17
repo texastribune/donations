e9379eb4b3385922b818075c535c293fa2cadfdb
AndrewGibson27 <atgibson27@gmail.com>
2019-06-17 16:49:54 -0500

    use api again

--- static/js/src/entry/account/store/modules/user.js
+++ static/js/src/entry/account/store/modules/user.js
@@ -13,8 +13,6 @@ import {
 } from '../../errors';
 import { PORTAL_API_URL } from '../../constants';
 
-import response from '../../dummy/response.json';
-
 function createDefaultState() {
   return {
     accessToken: '',
@@ -61,11 +59,11 @@ const actions = {
   },
 
   getUser: async ({ commit, state }) => {
-    /* const { data } = await axios.get(`${PORTAL_API_URL}self/`, {
+    const { data } = await axios.get(`${PORTAL_API_URL}self/`, {
       headers: { Authorization: `Bearer ${state.accessToken}` },
-    }); */
+    });
 
-    commit('SET_DETAILS', response);
+    commit('SET_DETAILS', data);
   },
 
   getTokenUser: ({ commit }) =>
