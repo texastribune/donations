1762be4d0fe020749f993d42889632f7141def83
AndrewGibson27 <atgibson27@gmail.com>
2019-06-18 14:56:37 -0500

    start configuring sentry releases

--- webpack/config.prod.js
+++ webpack/config.prod.js
@@ -1,12 +1,19 @@
 const webpack = require('webpack');
 const Merge = require('webpack-merge');
+const SentryWebpackPlugin = require('@sentry/webpack-plugin');
 
 const CommonConfig = require('./config.common.js');
 
 module.exports = Merge(CommonConfig, {
   mode: 'production',
 
+  devtool: 'source-map',
+
   plugins: [
     new webpack.HashedModuleIdsPlugin(),
+    new SentryWebpackPlugin({
+      release: 'foobarbaz',
+      include: 'static/build/',
+    }),
   ],
 });
