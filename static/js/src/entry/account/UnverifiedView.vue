8022152bc0e37aebf6c6a1d2d10cdd5f4e01b02c
AndrewGibson27 <atgibson27@gmail.com>
2019-06-25 10:51:50 -0500

    copy updates

--- static/js/src/entry/account/UnverifiedView.vue
+++ static/js/src/entry/account/UnverifiedView.vue
@@ -5,12 +5,14 @@
       <div class="l-minimal__content t-linkstyle--underlined">
         <h1 class="has-xl-btm-marg">Please verify your account</h1>
         <p class="t-space-heading-m">
-          You're logged in, but we still need to verify your email address
-          before you can view your Texas Tribune account. Check your inbox for
-          an email from The Texas Tribune with the subject line &quot;Please
-          confirm your account,&quot; then click to verify your email. Having
-          trouble? Contact
-          <a href="mailto:community@texastribune.org"
+          Thanks for creating a Texas Tribune account &mdash; you’re almost
+          done! To view your account, we need you to verify your email address:
+          <strong>{{ email }}</strong
+          >. Check your inbox for an email from The Texas Tribune with the
+          subject line &quot;Please confirm your account,&quot; then click to
+          verify your email. Having trouble? Contact
+          <a
+            href="mailto:community@texastribune.org?subject=account%20technical%20error"
             >community@texastribune.org</a
           >.
         </p>
@@ -21,8 +23,18 @@
 </template>
 
 <script>
+import tokenUser from './mixins/token-user';
+
 export default {
   name: 'UnverifiedView',
+
+  mixins: [tokenUser],
+
+  computed: {
+    email() {
+      return this.tokenUser.email;
+    },
+  },
 
   mounted() {
     window.onpopstate = () => {
--- static/js/src/entry/account/routes/home-exact/components/MessagesWrapper.vue
+++ static/js/src/entry/account/routes/home-exact/components/MessagesWrapper.vue
@@ -18,40 +18,33 @@
               <template v-if="isMDev">
                 Thanks for creating a Texas Tribune account. You can use this
                 login to comment on texastribune.org stories, view your donation
-                history or request a {{ lastYear }} tax receipt. Soon, you’ll
-                also be able to update your profile information and newsletter
-                preferences.
+                history or request a {{ lastYear }} tax receipt.
               </template>
               <template v-else-if="!isNeverGiven && isBlast">
                 Thanks for creating your Texas Tribune account! You can use this
                 login to comment on texastribune.org stories, view your donation
                 history, download your {{ lastYear }} tax receipt and view your
-                Blast payment history. Soon, you’ll also be able to update your
-                profile information and newsletter preferences.
+                Blast payment history.
               </template>
               <template v-else-if="!isNeverGiven">
                 Thanks for creating your Texas Tribune account! You can use this
                 login to comment on texastribune.org stories, view your donation
-                history and download a {{ lastYear }} tax receipt. Soon, you’ll
-                also be able to update your profile information and newsletter
-                preferences.
+                history and download a {{ lastYear }} tax receipt.
               </template>
               <template v-else-if="isBlast">
                 Thanks for creating a Texas Tribune account! You can use this
                 login to comment on texastribune.org stories and view your Blast
-                payment history. Soon, you’ll also be able to update your
-                profile information and newsletter preferences.
+                payment history.
               </template>
               <template v-else-if="isNeverGiven">
                 Thanks for creating a Texas Tribune account. You can use this
-                login to comment on texastribune.org stories. Soon, you’ll also
-                be able to update your profile information and newsletter
-                preferences.
+                login to comment on texastribune.org stories.
               </template>
+              Later this year, you’ll also be able to update your profile
+              information and newsletter preferences.
             </p>
             <p>
-              Need to make a change now? Click "account" anytime to reset your
-              password, or email
+              Need to make a change now or have a question? Email us at
               <a
                 href="mailto:community@texastribune.org"
                 ga-on="click"
@@ -59,9 +52,8 @@
                 :ga-event-action="ga.userPortal.actions['contact-us']"
                 :ga-event-label="ga.userPortal.labels.home"
               >
-                community@texastribune.org
-              </a>
-              with other questions.
+                community@texastribune.org </a
+              >.
             </p>
           </div>
         </template>
