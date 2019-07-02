8da84b54b83899cd6e3b260e0703e59635193dd8
AndrewGibson27 <atgibson27@gmail.com>
2019-07-02 14:38:27 -0500

    increase delay

--- static/js/src/entry/account/utils/auth-actions.js
+++ static/js/src/entry/account/utils/auth-actions.js
@@ -45,12 +45,12 @@ export const redirectAfterLogIn = () => {
   const url = localStorage.getItem(LOGIN_REDIRECT_KEY) || '/account';
   setTimeout(() => {
     window.location.href = url;
-  }, 1000);
+  }, 1800);
 };
 
 export const redirectAfterLogOut = () => {
   const url = localStorage.getItem(LOGOUT_REDIRECT_KEY) || '/account';
   setTimeout(() => {
     window.location.href = url;
-  }, 1000);
+  }, 1800);
 };
