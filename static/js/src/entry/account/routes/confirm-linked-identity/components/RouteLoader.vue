717d70e6e196b43375dd263855d7cb1edfe848ea
AndrewGibson27 <atgibson27@gmail.com>
2019-08-01 15:05:55 -0500

    better loading indicator

--- static/js/src/entry/account/routes/confirm-linked-identity/components/RouteLoader.vue
+++ static/js/src/entry/account/routes/confirm-linked-identity/components/RouteLoader.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="c-loading c-loading--s" />
+  <div>
+    <div class="c-loading c-loading--tight has-b-btm-marg" />
+    <p class="has-text-gray t-align-center t-size-m">
+      <strong>Grabbing your account information</strong>
+    </p>
+  </div>
 </template>
 
 <script>
