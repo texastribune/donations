5636c96b938962520435cfdd51a49ba177d099c4
AndrewGibson27 <atgibson27@gmail.com>
2020-02-27 13:00:58 -0600

    rm space

--- static/js/src/entry/account/routes/blast-payments/Index.vue
+++ static/js/src/entry/account/routes/blast-payments/Index.vue
@@ -9,7 +9,6 @@
     <div class="has-ump-side-padding has-xxl-btm-marg">
       <section class="c-detail-box c-detail-box--from-l">
         <div class="has-xxxl-btm-marg"><detail /></div>
-
         <internal-nav />
       </section>
     </div>
