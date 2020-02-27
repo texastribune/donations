24e4a281082d6c2f60dd45b4706d8b40314726de
AndrewGibson27 <atgibson27@gmail.com>
2020-02-27 12:58:51 -0600

    update email subject text

--- static/js/src/entry/account/link-email/components/LinkEmail.vue
+++ static/js/src/entry/account/link-email/components/LinkEmail.vue
@@ -13,7 +13,7 @@
         </p>
         <p class="t-size-xs has-text-gray">
           Check your inbox for an email from The Texas Tribune with the subject
-          line "Was this you?" and click to verify.
+          line "Link your account" and click to verify.
         </p>
       </template>
       <template v-else>
