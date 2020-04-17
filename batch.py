7d97ad8a805e59e51415e5e65693c9b975b66d89
tjb346 <tjb346@gmail.com>
2020-04-17 10:55:33 -0500

    add clack notifications on failed charge

--- batch.py
+++ batch.py
@@ -6,7 +6,7 @@ from pytz import timezone
 
 import celery
 import redis
-from charges import amount_to_charge, charge
+from charges import amount_to_charge, charge, ChargeException
 from npsp import Opportunity
 from util import send_email
 
@@ -100,7 +100,11 @@ def charge_cards():
         log.it(
             f"---- Charging ${amount} to {opportunity.stripe_customer} ({opportunity.name})"
         )
-        charge(opportunity)
+        try:
+            charge(opportunity)
+        except ChargeException:
+            # TODO should we alert slack? Did not because we had no notifications here before
+            pass
 
     log.send()
 
