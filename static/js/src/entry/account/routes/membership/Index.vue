5525220b153071c3be8189342242d1e3075dabbe
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2022-02-15 12:02:16 -0600

    Remove credit card message

--- static/js/src/entry/account/routes/membership/Index.vue
+++ static/js/src/entry/account/routes/membership/Index.vue
@@ -1,14 +1,5 @@
 <template>
   <div class="has-ump-top-padding">
-    <messages :num-messages="1">
-      <template v-slot:messages="{ setMessageSeen }">
-        <credit-card-message
-          :ga-label="ga.userPortal.labels.membership"
-          @setMessageSeen="setMessageSeen"
-        />
-      </template>
-    </messages>
-
     <h1 class="has-ump-side-padding has-xl-btm-marg t-size-xl">
       Your Membership
     </h1>
