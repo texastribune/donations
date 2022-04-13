8d88f91093788a2f30086478b9ff1d6c99f49f8e
ashley-hebler <ashleyhebler@gmail.com>
2022-04-13 15:16:13 -0500

    Add requested changes

--- static/js/src/Wall.vue
+++ static/js/src/Wall.vue
@@ -8,21 +8,20 @@
         :class="{ 'grid_separator--l': groupIndex !== memberGroups.length - 1 }"
         class="wall__group"
       >
-        <template v-if="group.members">
-          <h3 class="wall__heading grid_separator--s">{{ group.name }}</h3>
-          <ul class="wall__list">
-            <li
-              v-for="(member, memberIndex) in group.members"
-              :key="memberIndex"
-              class="wall__item"
-            >
-              <span class="wall__star c-icon c-icon--yellow c-icon--baseline"
-                ><svg aria-hidden="true"><use xlink:href="#star"></use></svg
-              ></span>
-              <slot name="member" :member="member"></slot>
-            </li>
-          </ul>
-        </template>
+        <h3 class="wall__heading grid_separator--s">{{ group.name }}</h3>
+        <slot name="subheading" :group="group"></slot>
+        <ul class="wall__list">
+          <li
+            v-for="(member, memberIndex) in group.members"
+            :key="memberIndex"
+            class="wall__item"
+          >
+            <span class="wall__star c-icon c-icon--yellow c-icon--baseline"
+              ><svg aria-hidden="true"><use xlink:href="#star"></use></svg
+            ></span>
+            <slot name="member" :member="member"></slot>
+          </li>
+        </ul>
       </section>
     </div>
   </section>
