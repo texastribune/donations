3b3143bacb9b8697ce8184dd843caf473a539749
matthewdylan <88053677+matthewdylan@users.noreply.github.com>
2023-08-02 11:02:12 -0500

    wrap amount_w_fees in float
    and simplify quarantine logic

--- server/app.py
+++ server/app.py
@@ -409,33 +409,21 @@ def add_stripe_donation(form=None, customer=None, donation_type=None, bad_actor_
     form = clean(form)
     period = form["installment_period"]
 
-    if period is None:
-        if quarantine:
-            contact = get_or_create_contact(form)
-            form["source"] = customer["id"]
-            form["amount_w_fees"] = amount_to_charge_stripe(form)
-            logging.info(f"amount_w_fees og: {form['amount_w_fees']}")
-            logging.info(f"amount_w_fees floating: {float(form['amount_w_fees'])}")
-            bad_actor_response.notify_bad_actor(
-                transaction=contact,
-                transaction_data=form
-            )
-            return True
+    if quarantine:
+        contact = get_or_create_contact(form)
+        form["source"] = customer["id"]
+        form["amount_w_fees"] = float(amount_to_charge_stripe(form))
+        bad_actor_response.notify_bad_actor(
+            transaction=contact,
+            transaction_data=form
+        )
+        return True
 
+    if period is None:
         logging.info("----Creating one time payment...")
         payment = create_payment_intent(customer=customer, form=form, quarantine=quarantine)
         return True
     else:
-        if quarantine:
-            contact = get_or_create_contact(form)
-            form["source"] = customer["id"]
-            form["amount_w_fees"] = amount_to_charge_stripe(form)
-            bad_actor_response.notify_bad_actor(
-                transaction=contact,
-                transaction_data=form
-            )
-            return True
-
         logging.info("----Creating recurring payment...")
         if donation_type == "membership":
             subscription = create_custom_subscription(customer=customer, form=form, quarantine=quarantine)
