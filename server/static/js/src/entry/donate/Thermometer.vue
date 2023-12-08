0e8c2a69e4fddd15208d17060eb9f18acaf44d20
Ashley Hebler <ashleyhebler@gmail.com>
2023-12-08 16:39:02 -0600

    feat(eoy): add end-of-year promo

--- server/static/js/src/entry/donate/Thermometer.vue
+++ server/static/js/src/entry/donate/Thermometer.vue
@@ -26,7 +26,7 @@ export default {
       error: false,
       data: [
         { slug: 'actual', label: '0', value: 0 },
-        { slug: 'goal', label: '500', value: 500 },
+        { slug: 'goal', label: '$100,000', value: 100000 },
       ],
     };
   },
@@ -43,7 +43,7 @@ export default {
 
   methods: {
     getSalesforceReport() {
-      const url = 'https://membership.texastribune.org/fmd2023.json';
+      const url = 'https://membership.texastribune.org/eoy2023.json';
 
       axios
         .get(url)
