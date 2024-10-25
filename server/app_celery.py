da5d07bacb62ced80f1eed4efcf97cf3e2c9fc64
matthewdylan <88053677+matthewdylan@users.noreply.github.com>
2024-10-25 15:46:22 -0500

    trying with cert_required
    yes

--- server/app_celery.py
+++ server/app_celery.py
@@ -11,7 +11,7 @@ def make_celery(app):
         broker=app.config["CELERY_BROKER_URL"],
         broker_connection_timeout=60,
         redis_backend_use_ssl = {
-            'ssl_cert_reqs': ssl.CERT_NONE,
+            'ssl_cert_reqs': ssl.CERT_REQUIRED,
         } if 'rediss' in REDIS_URL else {},
         result_backend=None,
         event_queue_expires=60,
